b931645ef4550fb3f500fcc64d73df0071c481e5
AlexandrShnipov <sshnipov@gmail.com>
2022-06-20 22:32:59 +0300

    commit gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -7,10 +7,11 @@ ghpages.publish(
         repo: 'https://github.com/AlexandrShnipov/calculator-Svelte.git', // Update to point to your repository
         user: {
             name: 'Alex', // update to use your name
-            email: 'sshnipov@gmail.com' // Update to use your email
-        }
+            email: 'alexandershnipov@gmail.com' // Update to use your email
+        },
+        dotfiles: true
     },
     () => {
-        console.log('Deploy Complete!')
+        console.log('Deploy Complete!');
     }
-)
+);
